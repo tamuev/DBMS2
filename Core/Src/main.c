a13fc64433a6967aafc8695bd55a8edc9558c971
Eli Nicksic <elinicksic@gmail.com>
2026-04-30 02:58:36 -0500

    Enable ABOM

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -264,7 +264,7 @@ static void MX_CAN1_Init(void)
   hcan1.Init.TimeSeg1 = CAN_BS1_11TQ;
   hcan1.Init.TimeSeg2 = CAN_BS2_2TQ;
   hcan1.Init.TimeTriggeredMode = DISABLE;
-  hcan1.Init.AutoBusOff = DISABLE;
+  hcan1.Init.AutoBusOff = ENABLE;
   hcan1.Init.AutoWakeUp = DISABLE;
   hcan1.Init.AutoRetransmission = DISABLE;
   hcan1.Init.ReceiveFifoLocked = DISABLE;
