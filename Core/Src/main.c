6141ced65aa1551be6a6165321fae0647999ddf3
Eli Nicksic <elinicksic@gmail.com>
2026-06-11 22:19:36 -0500

    Fix soc model saving on interrupt shutdown

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -693,17 +693,14 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
         GPIO_PinState pin_state = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_12);
         if (pin_state == GPIO_PIN_SET)
         {
-            dbms_ctx.qstats.historic_accumulated_loss += dbms_ctx.qstats.accumulated_loss;
-            dbms_ctx.qstats.accumulated_loss = 0;
+            // dbms_ctx.qstats.historic_accumulated_loss += dbms_ctx.qstats.accumulated_loss;
+            // dbms_ctx.qstats.accumulated_loss = 0;
             // CanLog(&dbms_ctx, "QD = %d\n", (uint32_t)(dbms_ctx.qstats.historic_accumulated_loss * 1000));
             // CanLog(&dbms_ctx, "bye\n");
-            dbms_ctx.qstats.initial = 99;//test
+            // dbms_ctx.qstats.initial = 99;//test
             dbms_ctx.stats.n_int_shutdowns++;
             WriteEEPROM(&dbms_ctx, EEPROM_DEBUG, &dbms_ctx.stats.n_int_shutdowns, 1);
-            // SaveQStats(&dbms_ctx);
-
-            //dbms_ctx.shutdown_requested = true;
-            //dbms_ctx.stats.shutdown_start_us = GetUs(&dbms_ctx);
+            SaveQStats(&dbms_ctx);
         }
         else
         {
