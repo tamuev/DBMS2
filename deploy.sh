a842d2a57803e7b823a29c9d5d0ae9f81e21af67
Eli Nicksic <elinicksic@gmail.com>
2026-09-04 16:49:36 -0500

    Fix build system

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 grep -rl 'fcyclomatic-complexity' --exclude=deploy.sh . | xargs -r sed -i 's/ -fcyclomatic-complexity//g'
-make -C Release all
+cmake --build build/Release
 
-arm-none-eabi-objcopy -O binary "Release/DBMS2.elf" "Release/DBMS2.bin"
-python3 upload.py Release/DBMS2.bin --device dbms
+arm-none-eabi-objcopy -O binary "build/Release/DBMS2.elf" "build/Release/DBMS2.bin"
+python3 upload.py build/Release/DBMS2.bin --device dbms
